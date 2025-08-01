735ce11c118e3a51a13b600d7d8d2f893c5c6f23
MAHADEV-J <j.a.browns.inbox@gmail.com>
2025-08-01 21:15:57 +0200

    Niet ProjectController, maar ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,14 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ProjectController extends AbstractController
+#[Route('/api', name: 'api_')]
+final class ProductController extends AbstractController
 {
-    #[Route('/project', name: 'app_project')]
+    #[Route('/products', name: 'app_product', methods:['get'])]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProjectController.php',
+            'path' => 'src/Controller/ProductController.php',
         ]);
     }
 }
