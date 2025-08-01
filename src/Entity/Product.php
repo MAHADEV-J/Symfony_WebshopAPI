1e522b908364e7068c10292083d6e2e153df63ec
MAHADEV-J <j.a.browns.inbox@gmail.com>
2025-08-01 20:33:14 +0200

    Entity, Migration toegevoegd

---
+++ src/Entity/Product.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $sku = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $price_excvar = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSku(): ?string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(string $sku): static
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    public function getPriceExcvar(): ?float
+    {
+        return $this->price_excvar;
+    }
+
+    public function setPriceExcvar(?float $price_excvar): static
+    {
+        $this->price_excvar = $price_excvar;
+
+        return $this;
+    }
+}
