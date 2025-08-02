031fd0a5b1e948fd5c53ce56b7bdca5493816930
MAHADEV-J <j.a.browns.inbox@gmail.com>
2025-08-02 17:52:44 +0200

    functie index() toegevoegd aan ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,16 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Product;
 
 #[Route('/api', name: 'api_')]
 final class ProductController extends AbstractController
 {
     #[Route('/products', name: 'app_product', methods:['get'])]
-    public function index(): JsonResponse
+    public function index(EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $products = $entityManager->getRepository(Product::class)->findAll();
+        
+        $data = [];
+        
+        foreach ($products as $product)
+        {
+             $data[] = [
+                 'id' => $product->getId(),
+                 'name' => $product->getName(),
+                 'sku' => $product->getSku(),
+                 'priceExcVar' => $product->getPriceExcvar(),
+                 'priceIncVar' => $product->getPriceExcvar() * 1.21
+             ];
+        }
+        
+        return $this->json($data);
     }
 }
