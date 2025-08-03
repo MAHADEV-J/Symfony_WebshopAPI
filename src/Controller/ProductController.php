f4a219bde7b87a503c0af38db5861f34f36e6b6c
MAHADEV-J <j.a.browns.inbox@gmail.com>
2025-08-03 14:27:56 +0200

    nog meer cleanup

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -17,7 +17,7 @@ final class ProductController extends AbstractController
     
     public function __construct(CartService $cartService)
     {
-         $this->cartService = $cartService;
+        $this->cartService = $cartService;
     }
     
     #[Route('/products', name: 'app_product', methods:['get'])]
@@ -27,15 +27,15 @@ final class ProductController extends AbstractController
         
         $data = [];
         
-        foreach ($products as $product)
+        foreach($products as $product)
         {
-             $data[] = [
-                 'id' => $product->getId(),
-                 'name' => $product->getName(),
-                 'sku' => $product->getSku(),
-                 'priceExcVat' => $product->getPriceExcvat(),
-                 'priceIncVat' => round($product->getPriceExcvat() * 1.21, 2)
-             ];
+            $data[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'sku' => $product->getSku(),
+                'priceExcVat' => $product->getPriceExcvat(),
+                'priceIncVat' => round($product->getPriceExcvat() * 1.21, 2)
+            ];
         }
         
         return $this->json($data);
@@ -44,60 +44,55 @@ final class ProductController extends AbstractController
     #[Route('/cart/add', name: 'add_to_cart', methods:['post'])]
     public function addToCart(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-         try
-         {
-              //TODO: eerst valideren dat product daadwerkelijk bestaat
-              $product = $entityManager->getRepository(Product::class)
-                                       ->find($request->toArray()['id']);
+        try
+        {
+            $product = $entityManager->getRepository(Product::class)
+                                     ->find($request->toArray()['id']);
 
-              if(!$product)
-              {
-                   return $this->json([
-                       'error' => 'Dit product bestaat niet.'
-                   ], 404);
-              }
-              
-              //roep functie aan in Service (of zo) die producten valideert
-              //en dan in winkelwagentje zet
-              $this->cartService->addProduct($product, $request->toArray()['quantity']);
-              
-         
-              $cart = $this->cartService->getContents();
-              $data = [];
-         
-              //voeg winkelwagentje toe aan $data
-              foreach ($cart as $cart_itemId => $cart_item)
-              {
-                   if($cart_itemId != 'totalExcVat')
-                   {
-                       $data[] = [
-                       'name' => $cart_item['product']->getName(),
-                       'priceExcVat' => $cart_item['product']->getPriceExcvat(),
-                       'priceIncVat' => round($cart_item['product']->getPriceExcVat() * 1.21, 2),
-                       'quantity' => $cart_item['quantity'],
-                       'subtotalExcVat' => $cart_item['product']->getPriceExcVat() * $cart_item['quantity'],
-                       'subtotalIncVat' => round($cart_item['product']->getPriceExcVat() * 1.21 * $cart_item['quantity'], 2)
-                   ];
-                   }
-              }
-               
-              return $this->json(
-              [
-                  'message' => "{$product->getName()} toegevoegd aan winkelwagen. De inhoud van de winkelwagen is:",
-                  'cart' => [
-                      'products' => $data
-                  ],
-                  'totalExcVat' => $cart['totalExcVat'],
-                  'totalIncVat' => round($cart['totalExcVat'] * 1.21, 2)
-              ]);
-         }
-         catch (\Exception $e)
-         {
-              return $this->json(
-              [
-                  'error' => 'Kon product niet toevoegen aan winkelwagen',
-                  'message' => $e->getMessage()
-              ], 500);
-         }
+            if(!$product)
+            {
+                return $this->json([
+                    'error' => 'Dit product bestaat niet.'
+                ], 404);
+            }
+            
+            $this->cartService->addProduct($product, $request->toArray()['quantity']);
+        
+            $cart = $this->cartService->getContents();
+            $data = [];
+        
+            foreach($cart as $cart_itemId => $cart_item)
+            {
+                if($cart_itemId != 'totalExcVat')
+                {
+                    $data[] = [
+                        'name' => $cart_item['product']->getName(),
+                        'priceExcVat' => $cart_item['product']->getPriceExcvat(),
+                        'priceIncVat' => round($cart_item['product']->getPriceExcVat() * 1.21, 2),
+                        'quantity' => $cart_item['quantity'],
+                        'subtotalExcVat' => $cart_item['product']->getPriceExcVat() * $cart_item['quantity'],
+                        'subtotalIncVat' => round($cart_item['product']->getPriceExcVat() * 1.21 * $cart_item['quantity'], 2)
+                    ];
+                }
+            }
+             
+            return $this->json(
+            [
+                'message' => "{$product->getName()} toegevoegd aan winkelwagen. De inhoud van de winkelwagen is:",
+                'cart' => [
+                    'products' => $data,
+                    'totalExcVat' => $cart['totalExcVat'],
+                    'totalIncVat' => round($cart['totalExcVat'] * 1.21, 2)
+                ],
+            ]);
+        }
+        catch (\Exception $e)
+        {
+            return $this->json(
+            [
+                'error' => 'Kon product niet toevoegen aan winkelwagen',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 }
--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -16,46 +16,46 @@ class CartService
     
     public function addProduct(Product $product, int $quantity): void
     {
-         $session = $this->requestStack->getSession();
+        $session = $this->requestStack->getSession();
          
-         $cart = $session->get('cart', []);
+        $cart = $session->get('cart', []);
          
-         if(isset($cart[$product->getId()]))
-         {
-             $cart[$product->getId()]['quantity'] += $quantity;
-         }
-         else
-         {
-             $cart[$product->getId()] = [
-                 'product' => $product,
-                 'quantity' => $quantity
-             ];
-         }
+        if(isset($cart[$product->getId()]))
+        {
+            $cart[$product->getId()]['quantity'] += $quantity;
+        }
+        else
+        {
+            $cart[$product->getId()] = [
+                'product' => $product,
+                'quantity' => $quantity
+            ];
+        }
          
-         $session->set('cart', $cart);
+        $session->set('cart', $cart);
     }
     
     public function getContents(): array
     {
-         $session = $this->requestStack->getSession();
-         $cart = $session->get('cart', []);
-         if (!isset($cart['totalExcVat']))
-         {
-              $cart['totalExcVat'] = $this->calculateTotal($cart);
-         }
-         return $cart;
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+        if(!isset($cart['totalExcVat']))
+        {
+            $cart['totalExcVat'] = $this->calculateTotal($cart);
+        }
+        return $cart;
     }
     
     private function calculateTotal(array $cart): float
     {
-         $total = 0;
-         foreach ($cart as $itemId => $item)
-         {
-             if($itemId !== 'totalExcVat' && isset($item['product']) && isset($item['quantity']))
-             {
-                 $total += $item['product']->getPriceExcVat() * $item['quantity'];
-             }
-         }
-         return $total;
+        $total = 0;
+        foreach($cart as $itemId => $item)
+        {
+            if($itemId !== 'totalExcVat' && isset($item['product']) && isset($item['quantity']))
+            {
+                $total += $item['product']->getPriceExcVat() * $item['quantity'];
+            }
+        }
+        return $total;
     }
 }
